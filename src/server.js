f78e36f16e4697358cd901d4e02e4045c759123c
Caio Beneton <caio.beneton@gmail.com>
2022-12-20 21:42:53 -0300

    server.js and database config

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express, { json } from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(json())
+
+app.use((req, res) => {
+    res.status(404).send("Rota não encontrada!")
+})
+
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`Server running in port ${port}`))
